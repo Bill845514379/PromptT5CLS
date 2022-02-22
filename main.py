f9df94aedb500dd3f63774c31ec27feb20c46adb
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-02-22 16:03:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ for test_id in range(len(seeds)):
         batch_size=cfg['train_batch_size'],
         shuffle=True,
         num_workers=0,
-        drop_last=True
+        drop_last=False
     )
 
     loader_test = DataLoader(
