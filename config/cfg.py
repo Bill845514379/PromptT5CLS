84826b23af8d7c02cce692e4483b831c2e2d4a16
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-02-23 15:35:46 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -16,5 +16,5 @@ cfg = {
 path = {
     'neg_path': 'data/rt-polaritydata/neg_label.txt',
     'pos_path': 'data/rt-polaritydata/pos_label.txt',
-    't5_path': 't5-3b'
+    't5_path': 't5-large'
 }
